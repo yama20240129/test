6f2ab003bd7bc7a29fe71c1085f8b04a94cd6b8d
yamaguchi_ss <BP004_yamaguchi@software-s.co.jp>
2024-08-25 14:55:58 +0900

    first

---
+++ src/test/java/com/sample/techpit/sample_backend/SampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.techpit.sample_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
